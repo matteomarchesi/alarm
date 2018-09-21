f213831d475e0a00d09d32e835334c06dcc5d69a
Matteo Marchesi <matteo.marchesi71@gmail.com>
2018-09-21 17:07:10 +0200

    Add files via upload

---
+++ alarm.ino
@@ -0,0 +1,70 @@
+bool hwEnable = false;
+bool swEnable = true;
+bool movement = false;
+bool alarm = false;
+
+int pinHWdisable = A0;
+int pinPIR = A1;
+int ledGreen = 2;
+int ledYellow = 3;
+int ledRed = 4;
+
+
+int highPIR = 400;
+unsigned long alarmDelay = 30000;
+unsigned long toggleStart = 0;
+int LL = 400;
+int UL = 800;
+
+// MUST returns true if disable jack is NOT inserted (true = alarm on)
+bool hwEnablecheck(){
+  if ((analogRead(pinHWdisable) > LL) and (analogRead(pinHWdisable) < UL)){ //aaa and bbb to be defined
+    return false;
+  }else{
+    return true;
+  }
+}
+
+bool movecheck(){
+  if (analogRead(pinPIR) > highPIR){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+void setup(){
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+}
+
+void loop(){
+  hwEnable = hwEnablecheck();
+
+//  swEnable = SIMCHECK??
+  
+  bool prevMove = movement;
+  movement = movecheck();
+
+  if (movecheck and !prevMove){
+    toggleStart = millis();
+  }
+  
+  if (movecheck and prevMove){
+    unsigned long moveTime = millis();
+    if ((moveTime-toggleStart) >= alarmDelay){
+      movement = true;
+    }
+  }
+  
+  alarm = hwEnable and swEnable and movement;
+  
+  if (alarm) {
+    digitalWrite(ledRed,HIGH);
+  } else{
+    digitalWrite(ledRed,LOW);
+  }
+  
+}
+
