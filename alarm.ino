b326ab8d30af9aa76033d7878c39d84d8a1d3c6b
matteomarchesi <matteo.marchesi71@gmail.com>
2018-09-22 14:54:41 +0200

    with messages

--- alarm.ino
+++ alarm.ino
@@ -6,11 +6,11 @@ bool alarm = false;
 int pinHWdisable = A0;
 int pinPIR = A1;
 int ledGreen = 2;
-int ledYellow = 3;
+int ledBlue = 3;
 int ledRed = 4;
 
 
-int highPIR = 400;
+int highPIR = 300;
 unsigned long alarmDelay = 30000;
 unsigned long toggleStart = 0;
 int LL = 400;
@@ -23,6 +23,8 @@ bool hwEnablecheck(){
   }else{
     return true;
   }
+  Serial.print("HWdis\t");
+  Serial.println(analogRead(pinHWdisable));
 }
 
 bool movecheck(){
@@ -31,21 +33,45 @@ bool movecheck(){
   }else{
     return false;
   }
+  Serial.print("PIR\t");
+  Serial.println(analogRead(pinPIR));
 }
 
+
 void setup(){
+  Serial.begin(9600);
+  Serial.println("Alarm!");
   pinMode(ledGreen, OUTPUT);
-  pinMode(ledYellow, OUTPUT);
+  pinMode(ledBlue, OUTPUT);
   pinMode(ledRed, OUTPUT);
 }
 
 void loop(){
-  hwEnable = hwEnablecheck();
+//  hwEnable = hwEnablecheck();
+
+  if ((analogRead(pinHWdisable) > LL) and (analogRead(pinHWdisable) < UL)){ //aaa and bbb to be defined
+    hwEnable = false;
+  }else{
+    hwEnable = true;
+  }
 
 //  swEnable = SIMCHECK??
   
   bool prevMove = movement;
-  movement = movecheck();
+//  movement = movecheck();
+  if (analogRead(pinPIR) > highPIR){
+    movement = true;
+  }else{
+    movement = false;
+  }
+  
+  Serial.print("HWdis\t");
+  Serial.print(analogRead(pinHWdisable));
+  Serial.print("\t");
+  Serial.print("PIR\t");
+  Serial.println(analogRead(pinPIR));
+
+
 
   if (movecheck and !prevMove){
     toggleStart = millis();
@@ -59,6 +85,19 @@ void loop(){
   }
   
   alarm = hwEnable and swEnable and movement;
+
+  if (hwEnable) {
+    digitalWrite(ledGreen,HIGH);
+  } else {
+    digitalWrite(ledGreen,LOW);
+  }
+  
+  if (movement) {
+    digitalWrite(ledBlue,HIGH);
+  } else {
+    digitalWrite(ledBlue,LOW);
+  }
+
   
   if (alarm) {
     digitalWrite(ledRed,HIGH);
